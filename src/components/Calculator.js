4fade64e8c720fd5771da6e24dca4882406e12d4
Matt Zelenak <matt@zelenak.me>
2021-08-17 00:12:14 -0400

    log save while refactor from function to stateful classes

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,34 +2,105 @@ import React, { useState } from 'react';
 import Displayer from './Displayer';
 import Keypad from './Keypad';
 
+const mathOperands = ['+', '-', '*', '/'];
+
 export default function Calculator() {
+  const [displayCleared, setDisplayCleared] = useState(false);
   const [display, setDisplay] = useState('');
+  const [operand, setOperand] = useState(null);
+  const [leftSideValue, setLeftSideValue] = useState(null);
+  const [rightSideValue, setRightSideValue] = useState(null);
+
+  const setSelectedOperand = (buttonValue) => {
+    const buttons = document.querySelectorAll('#keypad button');
+    
+    buttons?.forEach((element) => {
+      if(element.innerText === buttonValue) {
+        element.classList.add('selected-operand');
+      }
+    });
+  };
+
+  const removeSelectedOperand = () => {
+    const buttons = document.querySelectorAll('#keypad button');
+    buttons?.forEach((element) => {
+      element.classList.remove('selected-operand');
+    });
+  };
+
+  const reset = () => {
+    setRightSideValue(null);
+    setOperand(null);
+    setLeftSideValue(null);
+    setDisplay('');
+    setDisplayCleared(false);
+  };
+
+  const setDisplayerValue = (buttonValue) => {
+    let newDisplayValue = display + buttonValue;
+      
+    if(rightSideValue !== null && leftSideValue === null && displayCleared === false) {
+      newDisplayValue = '' + buttonValue;
+      setDisplayCleared(true);
+    }
+
+    setDisplay(newDisplayValue);
+  };
 
   const handleButtonClick = (e) => {
-    const buttonValue = e.target.textContent.value;
-    
+    const buttonValue = e.target.innerText;
+    const stringToNumber = parseInt(display);
     switch(buttonValue) {
       case 'C':
-        setDisplay('');
+        reset();
         break;
-      case 'CE':
-        setDisplay(display.slice(0, -1));
+      case '=':
+        if(rightSideValue !== null && leftSideValue === null) {
+          setLeftSideValue(stringToNumber);
+        }
+
+        calculate();
         break;
-      // case '=':
-      //   calculate();
-      //   break;
       default:
-        setDisplay(display + buttonValue);
+        if(!mathOperands.includes(buttonValue)) {
+          setDisplayerValue(buttonValue);
+          return;
+        }
+
+        if(rightSideValue === null) {
+          setRightSideValue(stringToNumber);
+          setOperand(buttonValue);
+          setSelectedOperand(buttonValue);
+        }
     }
   };
 
-  // const calculate = () => {
-  //   try {
-  //     setDisplay((eval(display) || "" ) + "");
-  //   } catch(e) {
-  //     setDisplay('error');
-  //   }
-  // };
+  const calculate = () => {
+    try {
+      let computedValue = null;
+
+      switch(operand) {
+        case '+':
+          computedValue = rightSideValue + leftSideValue;
+          break;
+        case '-':
+          computedValue = rightSideValue - leftSideValue;
+          break;
+        case '*':
+          computedValue = rightSideValue * leftSideValue;
+          break;
+        case '/':
+          computedValue = rightSideValue / leftSideValue;
+          break;
+      }
+      
+      setDisplay(computedValue);
+      removeSelectedOperand();
+    } catch(e) {
+      reset();
+      setDisplay('ERROR');
+    }
+  };
   
   return (
     <div id="calculator">
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,36 +1,32 @@
 import React from 'react';
 
 export default function Keypad(props) {
-  const propsOnClick = (e) => {
-    this.props.onClick(e);
-  }
-
   return (
     <div id="keypad">
-      <button onClick={e => propsOnClick(e)}>CE</button>
-      <button onClick={e => propsOnClick(e)}>C</button>
+      <button onClick={e => props.onClick(e)}>CE</button>
+      <button onClick={e => props.onClick(e)}>C</button>
       <button></button>
       <button></button>
 
-      <button onClick={e => propsOnClick(e)}>1</button>
-      <button onClick={e => propsOnClick(e)}>2</button>
-      <button onClick={e => propsOnClick(e)}>3</button>
-      <button onClick={e => propsOnClick(e)}>+</button>
+      <button onClick={e => props.onClick(e)}>1</button>
+      <button onClick={e => props.onClick(e)}>2</button>
+      <button onClick={e => props.onClick(e)}>3</button>
+      <button onClick={e => props.onClick(e)}>+</button>
 
-      <button onClick={e => propsOnClick(e)}>4</button>
-      <button onClick={e => propsOnClick(e)}>5</button>
-      <button onClick={e => propsOnClick(e)}>6</button>
-      <button onClick={e => propsOnClick(e)}>-</button>
+      <button onClick={e => props.onClick(e)}>4</button>
+      <button onClick={e => props.onClick(e)}>5</button>
+      <button onClick={e => props.onClick(e)}>6</button>
+      <button onClick={e => props.onClick(e)}>-</button>
 
-      <button onClick={e => propsOnClick(e)}>7</button>
-      <button onClick={e => propsOnClick(e)}>8</button>
-      <button onClick={e => propsOnClick(e)}>9</button>
-      <button onClick={e => propsOnClick(e)}>*</button>
+      <button onClick={e => props.onClick(e)}>7</button>
+      <button onClick={e => props.onClick(e)}>8</button>
+      <button onClick={e => props.onClick(e)}>9</button>
+      <button onClick={e => props.onClick(e)}>*</button>
 
-      <button onClick={e => propsOnClick(e)}>.</button>
-      <button onClick={e => propsOnClick(e)}>0</button>
-      <button onClick={e => propsOnClick(e)}>=</button>
-      <button onClick={e => propsOnClick(e)}>/</button>
+      <button onClick={e => props.onClick(e)}>.</button>
+      <button onClick={e => props.onClick(e)}>0</button>
+      <button onClick={e => props.onClick(e)}>=</button>
+      <button onClick={e => props.onClick(e)}>/</button>
     </div>
   );
 };
